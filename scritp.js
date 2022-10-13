45a1379049995ac6ba2ad8dc2119ef73169c9351
antonioii <asergiocosta.as@gmail.com>
2022-10-13 00:08:27 -0300

    Set CSS and HTML first skeleton
    Redone the CSS and HTML to include flexbox and grid properties.
    Add the first colors and positions.

---
+++ scritp.js
@@ -0,0 +1,7 @@
+const listOfButtons = Array.from(document.querySelectorAll('.number'));
+let gridIdentifier;
+listOfButtons.map((button) => {
+    gridIdentifier = "_" + button.textContent;
+    button.style.setProperty('grid-area',`${gridIdentifier}`);
+});
+console.table(listOfButtons);
