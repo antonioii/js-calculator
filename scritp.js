283e282f49428d54daf33ebd3b29189a549096f0
antonioii <asergiocosta.as@gmail.com>
2022-10-21 21:56:35 -0300

    Add basic plain text in script.js
    Add the first variables and plain text explaining the basic
    algorithym idea on how the calculator should work.

--- scritp.js
+++ scritp.js
@@ -1,3 +1,14 @@
+//A list with a pair of entries
+const entryArray = new Array(2);
+
+//a variable which sets the current operator being used
+let curOperator = '';
+
+//each time a number is pressed, add it inside arr[0]
+    //till an operator button is pressed and assigned to the operator variable
+        //then start to add the number inside arr[1]
+//press equal button to make the calculation based on the currentOperator being used
+
 //JS to put grid-areas in all buttons in order to match with the CSS inside buttonsContainer
 const listOfButtons = Array.from(document.querySelectorAll('.number'));
 let gridIdentifier;
