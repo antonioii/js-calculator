a15df8427bd3882d1cf5d2869274ce62434252b4
antonioii <asergiocosta.as@gmail.com>
2022-10-23 16:54:24 -0300

    Add the 'press and display' an entry functionality
    Set the initial variables and functions to make a number to be displayed
    after its buttom is clicked. Also, the entry should reset after an
    operator is chosen.

--- scritp.js
+++ scritp.js
@@ -1,19 +1,56 @@
 //A list with a pair of entries
-const entryArray = new Array(2);
+const entryArray = new Array('','');
 
-//a variable which sets the current operator being used
+//All the number buttons
+const listOfButtons = Array.from(document.querySelectorAll('.number'));
+
+//A variable which sets the current operator being used
 let curOperator = '';
 
-//each time a number is pressed, add it inside arr[0]
+//The entry display field
+const entryDisplay = document.querySelector('.entryDisplay');
+
+//Read a number
+function getANumber(numberButtomId) {
+    return numberButtomId.match(/[0-9]{1,2}/g)[0];
+}
+
+//Print an entry
+function printEntry(text) {
+    entryDisplay.value = text;
+}
+
+//Each time a number is pressed, add it inside arr[0]
+listOfButtons.forEach(
+    (numberButtom) => {
+        numberButtom.addEventListener('click', () => {
+            //get the number pressed
+            let number = getANumber(numberButtom.id);
+            //if current operator pressed, add to arr[0], else arr[1]
+            if(curOperator === '') {
+                entryArray[0] += number;
+                printEntry(entryArray[0]);
+            } else {
+                entryArray[1] += number;
+                printEntry(entryArray[1])
+            }
+            console.table(entryArray);
+            console.log(number);
+        })
+    }
+);
+
+
+
     //till an operator button is pressed and assigned to the operator variable
         //then start to add the number inside arr[1]
 //press equal button to make the calculation based on the currentOperator being used
 
 //JS to put grid-areas in all buttons in order to match with the CSS inside buttonsContainer
-const listOfButtons = Array.from(document.querySelectorAll('.number'));
 let gridIdentifier;
 listOfButtons.map((button) => {
     gridIdentifier = "_" + button.textContent;
     button.style.setProperty('grid-area',`${gridIdentifier}`);
 });
-console.table(listOfButtons);
+
+//console.table(listOfButtons);
